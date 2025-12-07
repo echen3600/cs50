550c045fdee8863a75799ff386e982f8f5d5335e
Edward Chen <echen3600@gmail.com>
2025-12-07 23:36:05 +0200

    completed mario

---
+++ pset1/mario.py
@@ -0,0 +1,12 @@
+height = int(input('Enter height: '))
+
+print('')
+
+for i in range(1, height + 1):
+    print('#' * i)
+
+print('')
+
+for i in range(1, height + 1):
+    print(' ' * (height - i) + '#' * i + ' ' + '#' * i)
+
